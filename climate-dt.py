77be7861511a0c15b35e594c33b3e6b59629eb6d
majh <James.Hawkes@ecmwf.int>
2024-03-14 21:36:59 +0000

    make scripts work out-of-the-box

--- climate-dt.py
+++ climate-dt.py
@@ -3,8 +3,9 @@ from polytope.api import Client
 # You can pass your email and apikey here, or put them in ~/.polytopeapirc (as JSON)
 # You can also set POLYTOPE_USER_EMAIL and POLYTOPE_USER_KEY in your environment
 client = Client(address='polytope.apps.lumi.ewctest.link', 
-                    user_email='<YOUR EMAIL>', 
-                    user_key='<YOUR ECMWF API KEY>')
+                    # user_email='<YOUR EMAIL>', 
+                    # user_key='<YOUR ECMWF API KEY>' or '<YOUR_DESP_KEY>'
+                    )
 
 # Optionally revoke previous requests
 client.revoke('all')
