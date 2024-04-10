604eb8ac8501d41bceb4715a6c17e4a198f13dac
majh <James.Hawkes@ecmwf.int>
2024-04-10 14:53:50 +0100

    update polytope endpoint for destination-earth.eu URL

--- climate-dt.py
+++ climate-dt.py
@@ -2,7 +2,7 @@ from polytope.api import Client
 
 # You can pass your email and apikey here, or put them in ~/.polytopeapirc (as JSON)
 # You can also set POLYTOPE_USER_EMAIL and POLYTOPE_USER_KEY in your environment
-client = Client(address='polytope.apps.lumi.ewctest.link', 
+client = Client(address='polytope.lumi.apps.dte.destination-earth.eu', 
                     # user_email='<YOUR EMAIL>', 
                     # user_key='<YOUR ECMWF API KEY>' or '<YOUR_DESP_KEY>'
                     )
